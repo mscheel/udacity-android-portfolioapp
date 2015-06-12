188b24f30b06be051d9a4f1b487e894aab93d9c4
Mark Scheel <mscheel@itriagehealth.com>
2015-06-11 22:08:09 -0600

    remove unused menu code

--- app/src/main/java/com/example/udacity/appportfolio/MainActivity.java
+++ app/src/main/java/com/example/udacity/appportfolio/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.udacity.appportfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,27 +22,5 @@ public class MainActivity extends AppCompatActivity {
         mButton4 = (ToasterButton) findViewById(R.id.button4);
         mButton5 = (ToasterButton) findViewById(R.id.button5);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
