7c365213ee006548affdaf1c4d2fb903c4a7049e
Mark Scheel <mscheel@itriagehealth.com>
2015-06-13 07:37:52 -0600

    trying to get a click, toast test to work

--- app/src/androidTest/java/com/example/udacity/appportfolio/ApplicationTest.java
+++ app/src/androidTest/java/com/example/udacity/appportfolio/ApplicationTest.java
@@ -1,12 +1,15 @@
 package com.example.udacity.appportfolio;
 
-import android.support.test.espresso.assertion.ViewAssertions;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -27,11 +30,12 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
     }
 
     public void testSpotifyButtonIsOnScreen() {
-        try {
-            setUp();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        onView(withText(R.string.spotify_streamer_button_label)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText(R.string.spotify_streamer_button_label)).check(matches(isDisplayed()));
+    }
+
+    public void testToastDoesDisplayWhenSpotifyButtonIsClicked() {
+        onView(withText(R.string.spotify_streamer_button_label)).inRoot(
+                withDecorView(not(getActivity().getWindow().getDecorView()))).perform(click()).check(
+                matches(isDisplayed()));
     }
 }
