51ef2ae82881c27b7e2ecb121b06d7c6a02f2fc7
Mark Scheel <mscheel@itriagehealth.com>
2015-06-13 00:01:50 -0600

    big commit - 1. improve theme for recent color and action bar color 2. add in app icon, used roman nurik generator, 3. refactor activity to use better less generic button variable names 4. stop using ToasterButton custom view because couldn't get to ripple 5. make buttons ripple on API 21+ and have states on lower api level 6. work on adding an espresso test but can't get to work

--- app/src/main/java/com/example/udacity/appportfolio/MainActivity.java
+++ app/src/main/java/com/example/udacity/appportfolio/MainActivity.java
@@ -2,25 +2,35 @@ package com.example.udacity.appportfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ToasterButton mButton1;
-    ToasterButton mButton2;
-    ToasterButton mButton3;
-    ToasterButton mButton4;
-    ToasterButton mButton5;
+    Button mButtonSpotify;
+    Button mButtonScores;
+    Button mButtonLibrary;
+    Button mButtonBigger;
+    Button mButtonXyz;
+    Button mButtonCapstone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mButton1 = (ToasterButton) findViewById(R.id.button1);
-        mButton2 = (ToasterButton) findViewById(R.id.button2);
-        mButton3 = (ToasterButton) findViewById(R.id.button3);
-        mButton4 = (ToasterButton) findViewById(R.id.button4);
-        mButton5 = (ToasterButton) findViewById(R.id.button5);
+        mButtonSpotify = (Button) findViewById(R.id.button_spotify);
+        mButtonScores = (Button) findViewById(R.id.button_scores);
+        mButtonLibrary = (Button) findViewById(R.id.button_library);
+        mButtonBigger = (Button) findViewById(R.id.button_bigger);
+        mButtonXyz = (Button) findViewById(R.id.button_xyz);
+        mButtonCapstone = (Button) findViewById(R.id.button_capstone);
 
     }
+
+    public void buttonClick(View v) {
+        Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
+    }
+
 }
